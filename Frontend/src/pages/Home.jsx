6cc4aecdc570a5e439b36e699c1496f2baf9fefc
ramya2623 <ramya596122@gmail.com>
2026-08-14 19:46:42 +0530

    Fix Home CSS import for deployment

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import "../styles/home.css";
+import "../styles/Home.css";
 import {
   FolderOpen,
   MessageSquare,
